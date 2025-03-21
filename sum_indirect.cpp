e79aaf39c3f7e340adaa4b60f06d4f9f5df0361e
M0NIQUEE <moniquenguyen425@gmail.com>
2025-03-20 20:12:26 -0700

    feat: sum implementation

--- sum_indirect.cpp
+++ sum_indirect.cpp
@@ -11,12 +11,21 @@
 void 
 setup(int64_t N, double A[])
 {
+   srand48(time(nullptr));
+
+   for (int64_t i = 0; i < N; i++) {
+       A[i] = lrand48() % N;
+   }
    printf(" inside sum_indirect problem_setup, N=%lld \n", N);
 }
 
 double
 sum(int64_t N, double A[])
 {
+   double sum = 0.0;
+   for(int i = 0; i < N; i ++){
+      sum += A[i];
+   }
    printf(" inside sum_indirect perform_sum, N=%lld \n", N);
 
    return 0;
