65109ea7b9f9aac79ad6a696a4604a5782116a59
M0NIQUEE <moniquenguyen425@gmail.com>
2025-03-20 20:00:55 -0700

    test

--- sum_direct.cpp
+++ sum_direct.cpp
@@ -5,12 +5,20 @@
 #include <random>
 #include <vector>
 #include <string.h>
+#include <cstdlib>
+#include <cmath>
 
 #include "sums.h"
 
 void 
 setup(int64_t N, double A[])
 {
+   srand48(time(nullptr));
+
+   for (int64_t i = 0; i < N; i++) {
+       A[i] = lrand48() % N; // Random number in the range [0, N-1]
+   }
+
    printf(" inside direct_sum problem_setup, N=%lld \n", N);
 }
 
